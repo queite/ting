bbd842d9262ba5f5fb713a9c67ae00f5334c6966
Queite <queitesc@gmail.com>
2022-12-01 17:58:09 -0300

    refactor: :recycle: refatora funções  de word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,22 +1,34 @@
-def exists_word(word, instance):
+def get_occurrences(word, instance):
     result = []
 
     for dict in instance:
         occurrences = []
         for index, line in enumerate(dict['linhas_do_arquivo']):
             if word.lower() in line.lower():
-                occurrences.append({"linha": index + 1})
+                occurrences.append({
+                    'linha': index + 1,
+                    'conteudo': line
+                })
 
         if len(occurrences):
             data = {
-                "palavra": word,
-                "arquivo": dict['nome_do_arquivo'],
-                "ocorrencias": occurrences
+                'palavra': word,
+                'arquivo': dict['nome_do_arquivo'],
+                'ocorrencias': occurrences
             }
             result.append(data)
+    return result
+
+
+def exists_word(word, instance):
+    result = get_occurrences(word, instance)
+
+    for dict in result:
+        for occurrence in dict['ocorrencias']:
+            del occurrence['conteudo']
 
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return get_occurrences(word, instance)
