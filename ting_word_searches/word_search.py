59f8af80960f5e8488c66cfd82105145bb66d3e5
Queite <queitesc@gmail.com>
2022-12-01 17:16:32 -0300

    feat: :sparkles: implementa função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for dict in instance:
+        occurrences = []
+        for index, line in enumerate(dict['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
+
+        if len(occurrences):
+            data = {
+                "palavra": word,
+                "arquivo": dict['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            }
+            result.append(data)
+
+    return result
 
 
 def search_by_word(word, instance):
