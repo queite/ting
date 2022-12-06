79c8dea08e383f0beb93931e7c2e9f04165e74cc
Queite <queitesc@gmail.com>
2022-12-06 07:36:01 -0300

    refactor: :recycle: refatora word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,10 +1,21 @@
 def exists_word(word, instance):
-    result = search_by_word(word, instance)
+    result = []
 
-    for dict in result:
-        for occurrence in dict['ocorrencias']:
-            del occurrence['conteudo']
+    for dict in instance:
+        occurrences = []
+        for index, line in enumerate(dict['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurrences.append({
+                    'linha': index + 1
+                })
 
+        if len(occurrences):
+            data = {
+                'palavra': word,
+                'arquivo': dict['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            }
+            result.append(data)
     return result
 
 
